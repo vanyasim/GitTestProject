4d71681d3c1ca3c75996cab62ee3b93eefd8d6a7
vanyasim <simdevcom@gmail.com>
2014-12-16 23:35:39 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/admin/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/admin/gittestproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String s = "";
+        String s = "2";
     }
 
 
